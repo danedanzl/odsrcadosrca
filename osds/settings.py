480013f7b82b0596a91e2498f0bb020062235bd4
Bor Grošelj Simić <bgs@turminal.net>
2023-11-22 01:24:36 +0100

    generate urls and add a script for qr codes

--- osds/settings.py
+++ osds/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'common.apps.CommonConfig',
+    'naloge.apps.NalogeConfig',
+    'konzola.apps.KonzolaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
