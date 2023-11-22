fb074743265f04aa7050cf3b660cf1201f8be533
Jakob Schrader <jakob.schrader@guest.arnes.si>
2023-11-22 13:21:14 +0100

    Add Mchoice models

--- naloge/admin.py
+++ naloge/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Task, Attempt
-
-# Register your models here.
+from .models import Task, Attempt, MchoiceTask, MchoiceQuestion, MchoiceAnswer
 
 admin.site.register(Task)
 admin.site.register(Attempt)
+
+admin.site.register(MchoiceTask)
+admin.site.register(MchoiceQuestion)
+admin.site.register(MchoiceAnswer)
 
