acf5feffa626b99058e6673c9a2e7a330695b868
Bor Grošelj Simić <bgs@turminal.net>
2023-11-24 22:50:35 +0100

    map KTs to tasks

--- common/views.py
+++ common/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
 
-# Create your views here.
+from naloge.views import task_map
+from naloge.task import Task
 
 def index(request):
     # TODO: display login info for logged in users somewhere
     return render(request, "common/homepage.html")
+
+def kt(request, kt):
+    if name := task_map.get(kt):
+        # TODO: pick difficulty based in request.user
+        return redirect(Task(name[1], "").info_viewname)
+    else:
+        raise Http404
