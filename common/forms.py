a2b548778d2f4659a1515d80f69bee6d3fdc4385
Bor Grošelj Simić <bgs@turminal.net>
2023-11-25 23:27:09 +0100

    registration improvements

--- common/forms.py
+++ common/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 
-gopts = [" ", "Rešujem sam"]+[str(i) for i in range(2,11)] + ["10 - 15", "Več kot 15"]
+gopts = ["", "Rešujem sam"]+[str(i) for i in range(2,11)] + ["10 - 15", "Več kot 15"]
 gopts = list(zip(gopts, gopts))
 
-sopts = [" ", "6 - 11 let", "12 - 15 let", "16 - 21", "22 - 30", "31 - 45", "46 - 65", "66 - ∞"]
+sopts = ["", "6 - 11 let", "12 - 15 let", "16 - 21", "22 - 30", "31 - 45", "46 - 65", "66 - ∞"]
 sopts = list(zip(sopts, sopts))
 
 class Register(forms.Form):
-    email = forms.CharField(label="E-poštni naslov", max_length=200, widget=forms.TextInput()) # TODO validator?
+    email = forms.CharField(label="E-poštni naslov", max_length=200,
+                            widget=forms.TextInput(), required=False) # TODO validator?
     group = forms.CharField(label="Boste reševali kot skupina? Če da, koliko vas je?", max_length=30,
-                            widget=forms.Select(choices=gopts))
-    starost = forms.CharField(label="Približna starost večine udeležencev?", max_length=30,
-                              widget=forms.Select(choices=sopts))
+                            widget=forms.Select(choices=gopts), required=False)
+    starost = forms.CharField(label="Koliko si star (če vas je več, kaj je najpogostejša starost v skupini)?", max_length=30,
+                              widget=forms.Select(choices=sopts), required=False)
