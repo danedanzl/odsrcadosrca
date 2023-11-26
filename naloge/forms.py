bd07d1199bfb2894e76028f54e24254d5f15baf9
Jakob Schrader <jakob.schrader@guest.arnes.si>
2023-11-26 11:29:18 +0100

    Clean up forms (deleting prints)

--- naloge/forms.py
+++ naloge/forms.py
@@ -16,8 +16,6 @@ opts = sorted(["",
         "mrežico", "svetilo", "hladno vodo", "30 minut", "prevelik",
         "sladoled", "Sonce", "prevroč", "opekel zaradi trenja", "15 minut",
         "zažganem", "topli čaj", "osmodil", "šoku", "zardel", "pustil počivati", "ožganega" ])
-print(opts)
-print(list(zip(opts,opts)))
 assert len(opts) == 18
 
 s = ["Sonce", "svetilo", "opekel zaradi trenja", "šoku", "hladno vodo", "15 minut",
@@ -43,7 +41,6 @@ class NNZOpekline(forms.Form):
 
     def correct(self):
         r = [self.cleaned_data[f"f{i}"] for i in range(1, len(s)+1)]
-        print(len(s), len(r), r)
         assert len(r) == len(s)
         return { 'correct' : s == r,
                 'sol': ( {f"f{i}" : s[i-1] for i in range(1, len(s)+1)}
@@ -58,8 +55,6 @@ class OBVImobilizacija(forms.Form):
     r2 = forms.CharField(max_length=40, widget=forms.RadioSelect(choices=(('Leva fotografija','Leva fotografija'), ('Desna fotografija','Desna fotografija'))))
 
     def correct(self):
-        print(self.cleaned_data['r1'])
-        print(self.cleaned_data['r2'])
         d = { 'first' : self.cleaned_data['r1'] == 'Leva fotografija',
              'second' : self.cleaned_data['r2'] == 'Desna fotografija' }
         return d | { 'correct' : d['first'] and d['second'] }
