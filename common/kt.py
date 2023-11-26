717028806e7877e48d2be9a5a1a81ba601b954f7
Bor Grošelj Simić <bgs@turminal.net>
2023-11-26 05:32:10 +0100

    add hints/locations

--- common/kt.py
+++ common/kt.py
@@ -3,15 +3,13 @@ from django.urls import path, reverse
 from django.shortcuts import render, redirect
 
 class KT:
-    def __init__(self, urlid, imgid, tasks, tip=None, location=None):
+    def __init__(self, urlid, imgid, tasks, tip, location):
         self.urlid = urlid
         self.imgid = imgid
         self.obv = tasks[0]
         self.nnz = tasks[1]
-        if tip is None:
-            self.tip = "Tvoja mami"
-        if location is None:
-            self.location = "Brokopondo"
+        self.tip = tip
+        self.location = location
 
     def __lt__(self, other):
         return self.urlid < other.urlid
