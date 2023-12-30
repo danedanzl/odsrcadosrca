3347ebc7fa999ad42aa534f6f2f043efa326390d
Bor Grošelj Simić <bgs@turminal.net>
2023-12-30 03:17:08 +0100

    turn off stuff

--- common/urls.py
+++ common/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = [
 	path("", views.index, name="index"),
-    path("<int:kt>", views.kt, name="kt"),
-    path("i/<int:imgid>", views.img, name="img"),
-    path("cheat/<int:imgid>", views.cheat, name="cheat"),
-    path("list", views.ktlist, name="ktlist"),
-    path("group", views.group_selection, name="group_selection"),
-    path("nnz", views.pick_nnz, name="nnz"),
-    path("obv", views.pick_obv, name="obv"),
-    path("reset", views.cookie_reset, name="reset"),
-    path("start", views.register, name="register"),
+    #path("<int:kt>", views.kt, name="kt"),
+    #path("i/<int:imgid>", views.img, name="img"),
+    #path("cheat/<int:imgid>", views.cheat, name="cheat"),
+    #path("list", views.ktlist, name="ktlist"),
+    #path("group", views.group_selection, name="group_selection"),
+    #path("nnz", views.pick_nnz, name="nnz"),
+    #path("obv", views.pick_obv, name="obv"),
+    #path("reset", views.cookie_reset, name="reset"),
+    #path("start", views.register, name="register"),
 ]
