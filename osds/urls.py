e7ef5f82be87827bf2aa826343e6047fff7d8e4a
Bor Grošelj Simić <bgs@turminal.net>
2024-02-13 19:37:56 +0100

    restart

--- osds/urls.py
+++ osds/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
 #   path('admin/', admin.site.urls),
-#    path('naloge/', include("naloge.urls")),
+    path('naloge/', include("naloge.urls")),
     path('', include('common.urls')),
 ]
